5a20e100c5ff0d1ac54709c38475c8cd30dde0a1
dav-123 <david.he07@yahoo.ca>
2021-10-20 15:47:02 -0400

    included NOT NULL for reservations create table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -44,8 +44,8 @@ CREATE TABLE reservations (
     id SERIAL PRIMARY KEY NOT NULL,
     property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
     guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    start_date DATE,
-    end_date DATE
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
 
 );
 
