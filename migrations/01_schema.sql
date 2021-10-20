a7eae7630afe861f3a5e078e10f604cb05eca314
dav-123 <david.he07@yahoo.ca>
2021-10-20 15:35:30 -0400

    made minor adjustments to datatypes of properties of the entities

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -13,18 +13,18 @@ CREATE TABLE properties (
     id SERIAL PRIMARY KEY NOT NULL,
     owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     title VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    cost_per_night VARCHAR(32),
-    parking_spaces SMALLINT,
-    number_of_bedrooms SMALLINT,
-    number_of_bathrooms SMALLINT,
+    description TEXT,
     thumbnail_photo_url VARCHAR(255),
     cover_photo_url VARCHAR(255),
-    country TEXT,
-    street TEXT,
-    city TEXT,
-    province TEXT, 
-    post_code TEXT,
+    cost_per_night INTEGER,
+    parking_spaces INTEGER,
+    number_of_bedrooms INTEGER,
+    number_of_bathrooms INTEGER,
+    country VARCHAR(255),
+    street VARCHAR(255),
+    city VARCHAR(255),
+    province VARCHAR(255), 
+    post_code VARCHAR(255),
     active BOOLEAN DEFAULT TRUE
 
 );
@@ -48,7 +48,7 @@ CREATE TABLE property_reviews (
     guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,    
     message TEXT,
-    rating INTEGER
+    rating SMALLINT
 
 );
 
