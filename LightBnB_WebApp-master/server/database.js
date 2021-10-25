973c81ceb24d013616866721565188ddd12b7a88
dav-123 <david.he07@yahoo.ca>
2021-10-25 11:16:46 -0400

    included getClient lines of code using pool.connect in database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -32,12 +32,21 @@ const { Pool } = require('pg');
       return pool.query(text, params, (err, res) => {
 
         const duration = Date.now() - start
-        console.log('executed query', { text, duration, rows: res.rowcount })
+        console.log('executed query', { text, duration, rows: res.rowCount })
 
         callback(err, res)
 
       })
     },
+
+      getClient: (callback) => {
+
+        pool.connect((err, client, done) => {
+
+          callback(err, client, done)
+
+        })
+      }
   }
 
 /// Users
