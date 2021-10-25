0c2ca861446839af9eea2970cfbd1a9d498deabd
dav-123 <david.he07@yahoo.ca>
2021-10-25 12:11:55 -0400

    accecpted incoming change for return db in database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -221,12 +221,7 @@ const addProperty = function(property) {
 
   // console.log("ANSWER1111: ", property);
 
-<<<<<<< HEAD
-  return pool
-=======
-
   return db
->>>>>>> project_structure
   .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
         RETURNING *;`, 
         [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
